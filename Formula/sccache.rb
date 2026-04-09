4468f19c6b9d32d4f13ef88cfdbfdbaa55cbecaa
Michael Gunawan <michael.gunawan@safetyculture.io>
2026-04-10 05:57:25 +1000

    Add sccache formula v0.14.1
    SafetyCulture fork with multi-level cache support (disk + S3).

---
+++ Formula/sccache.rb
@@ -0,0 +1,21 @@
+class Sccache < Formula
+  desc "sccache with multi-level cache support (SafetyCulture fork)"
+  homepage "https://github.com/scmichaelg/sccache"
+  version "0.14.1"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/scmichaelg/sccache/releases/download/v0.14.1/sccache-v0.14.1-aarch64-apple-darwin.tar.gz"
+      sha256 "37c05ccdb17b99e0841167b3c366a8aa47e04d407aa88ad47179dfc5bcdeba66"
+    end
+  end
+
+  def install
+    bin.install "sccache"
+  end
+
+  test do
+    assert_match "sccache", shell_output("#{bin}/sccache --version")
+  end
+end
