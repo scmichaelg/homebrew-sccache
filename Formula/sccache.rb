1491dcf67c938a2f2225def092ce304902474872
Michael Gunawan <michael.gunawan@safetyculture.io>
2026-05-21 16:45:14 +1000

    chore: bump sccache to v0.15.3 (macOS + Linux, arm64 + x86_64)

--- Formula/sccache.rb
+++ Formula/sccache.rb
@@ -1,13 +1,28 @@
 class Sccache < Formula
   desc "sccache with multi-level cache support (SafetyCulture fork)"
   homepage "https://github.com/scmichaelg/sccache"
-  version "0.14.1"
+  version "0.15.3"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/scmichaelg/sccache/releases/download/v0.14.1/sccache-v0.14.1-aarch64-apple-darwin.tar.gz"
-      sha256 "85849b19373c8fce97cd2178f08ad1b7ebf18d4c12184c94f00b381661168f48"
+      url "https://github.com/scmichaelg/sccache/releases/download/v0.15.3/sccache_0.15.3_darwin_arm64.tar.gz"
+      sha256 "f7460aa465dc47a07e0fd149efd79085bd7ff1a38ad06cb89c1dda04becce4e3"
+    end
+    on_intel do
+      url "https://github.com/scmichaelg/sccache/releases/download/v0.15.3/sccache_0.15.3_darwin_amd64.tar.gz"
+      sha256 "e647346a8441647e2dd1bb835540eee8161bf32e5342b5da00cb335ebf39343c"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/scmichaelg/sccache/releases/download/v0.15.3/sccache_0.15.3_linux_arm64.tar.gz"
+      sha256 "830aadb3dcaf21bd7e7820530c3a85e5a404d8988085e705820a14d5894199cb"
+    end
+    on_intel do
+      url "https://github.com/scmichaelg/sccache/releases/download/v0.15.3/sccache_0.15.3_linux_amd64.tar.gz"
+      sha256 "4c69c9b259c3d97d8d238687e2d650e27ca3cba0811c7a0327a4780727c186f2"
     end
   end
 
